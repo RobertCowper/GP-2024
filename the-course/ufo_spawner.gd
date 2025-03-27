bc8d15305141d03ed5bfaefbc3f4aeb0a550131b
RobertCowper <55623373+RobertCowper@users.noreply.github.com>
2025-03-27 12:16:29 +0000

    Updates

--- the-course/ufo_spawner.gd
+++ the-course/ufo_spawner.gd
@@ -22,7 +22,7 @@ func spawn():
 		var x = sin(theta) * radius
 		var y = - cos(theta) * radius
 		var ufo = ufo_scene.instantiate()
-		# ufo.color = color
+		ufo.color = color
 		ufo.position = position + Vector2(x, y)		
 		get_parent().add_child(ufo)
 		
